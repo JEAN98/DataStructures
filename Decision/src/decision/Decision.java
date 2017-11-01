e120a32d4d6dacedb0dbe014452e423c45e19724
Unknown <kmal.kenneth@live.com>
2017-10-31 22:06:54 -0600

    update ui system

--- Decision/src/decision/Decision.java
+++ Decision/src/decision/Decision.java
@@ -5,9 +5,10 @@
  */
 package decision;
 
-import decision.ui.Login;
+import decision.controllers.LoginController;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -21,36 +22,42 @@ public class Decision extends Application {
     //UI
     private BorderPane root;
     private Scene scene;
-    private Login login;
-    
+
     //System
     private UserControl userControl;
-    
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        
-        initUI();
-        initSystem();
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
         
-        login.start(this);
-    }
-    
-    private void initUI(){
-    
+
         root = new BorderPane();
-        login = new Login();
-        root.setCenter(login);
+        initSystem();
+        initUILogin();
+        
         scene = new Scene(root, 720, 480);
         scene.getStylesheets().add("/decision/means/bootstrap3.css");
         scene.getStylesheets().add("/decision/means/DecisionStyles.css");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
-    
-    private void initSystem(){
-    
+
+    private void initUILogin() throws IOException {
+        
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Decision.class.getResource("/decision/means/Login.fxml"));
+        BorderPane start = (BorderPane) loader.load();
+        root.setCenter(start);
+        
+        LoginController loginController = loader.getController();
+        loginController.start(this);
+
+    }
+
+    private void initSystem() {
+
         userControl = new UserControl();
     }
 
@@ -75,14 +82,6 @@ public class Decision extends Application {
 
     public void setScene(Scene scene) {
         this.scene = scene;
-    }
-
-    public Login getLogin() {
-        return login;
-    }
-
-    public void setLogin(Login login) {
-        this.login = login;
     }
 
     public UserControl getUserControl() {
---
+++ Decision/src/decision/controllers/LoginController.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decision.controllers;
+
+import decision.Decision;
+import decision.Sex;
+import decision.User;
+import decision.UserControl;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import org.controlsfx.control.Notifications;
+
+/**
+ * FXML Controller class
+ *
+ * @author kenma
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private BorderPane root_pane;
+    @FXML
+    private TextField loginDNI;
+    @FXML
+    private TextField loginPassword;
+    @FXML
+    private Button loginButtom;
+    @FXML
+    private TextField singupDNI;
+    @FXML
+    private TextField singupFullName;
+    @FXML
+    private TextField singupAge;
+    @FXML
+    private RadioButton singupMale;
+    @FXML
+    private ToggleGroup sex;
+    @FXML
+    private RadioButton singupFemale;
+    @FXML
+    private TextField singupPassword;
+    @FXML
+    private TextField singupConfirmPassword;
+    @FXML
+    private Button singupButtom;
+    
+    private Decision decision;
+    private UserControl userControl;
+    
+    public void start(Decision decision) {
+
+        this.decision = decision;
+        this.userControl = this.decision.getUserControl();
+
+        loginButtom.requestFocus();
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void logIn(ActionEvent event) {
+         if (userControl.login(loginDNI.getText(), loginPassword.getText())) {
+
+            System.out.println("Login");
+        }
+    }
+
+    @FXML
+    private void SingUp(ActionEvent event) {
+        if (singupPassword.getText().equals(singupConfirmPassword.getText())) {
+
+            Sex sexUser = null;
+
+            if (singupMale == this.sex.getSelectedToggle()) {
+
+                sexUser = Sex.Male;
+            } else {
+
+                sexUser = Sex.Female;
+            }
+
+            User newUser = new User(
+                    singupFullName.getText(),
+                    singupDNI.getText(),
+                    Short.parseShort(singupAge.getText()),
+                    sexUser,
+                    singupPassword.getText()
+            );
+
+            userControl.addUser(newUser);
+            
+            Notifications.create()
+                    .title("Sing Up")
+                    .text("Successful registration.")
+                    .position(Pos.TOP_RIGHT)
+                    .showConfirm();
+        } else {
+
+            Notifications.create()
+                    .title("Sing Up")
+                    .text("Passwords do not match.")
+                    .position(Pos.TOP_RIGHT)
+                    .showError();
+        }
+    }
+}
